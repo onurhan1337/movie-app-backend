f2f5bc3750ed8d348d7765673e5b104512e1814d
Onurhan1337 <onurhandtr@gmail.com>
2022-07-03 15:56:34 +0300

    Features completed for heroku deploy

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/onurhan1337/movie-app-backend/configs"
 	"github.com/onurhan1337/movie-app-backend/routes"
 )
@@ -9,11 +12,22 @@ import (
 func main() {
 	app := fiber.New()
 
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
 	// run database
 	configs.ConnectDB()
 
 	// routes
 	routes.Route(app)
 
-	app.Listen(":8080")
+	// Run on Local Server.
+	// In any need-cases you can comment line 30 (deployment running),
+	// and uncomment 27 (local running).
+
+	// Warning: Code shouldn't be pushed if app is running on local)
+
+	// log.Fatal(http.ListenAndServe(":8080", router))
+
+	app.Listen(":" + port)
 }
