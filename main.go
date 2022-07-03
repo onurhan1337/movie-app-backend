a27ee2c54debc94b693faf73d3bc5e364eb96ca5
Onurhan1337 <onurhandtr@gmail.com>
2022-07-03 17:16:55 +0300

    Heroku deploy test

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/onurhan1337/movie-app-backend/configs"
 	"github.com/onurhan1337/movie-app-backend/routes"
 )
@@ -9,11 +12,14 @@ import (
 func main() {
 	app := fiber.New()
 
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
 	// run database
 	configs.ConnectDB()
 
 	// routes
 	routes.Route(app)
 
-	app.Listen(":8080")
+	app.Listen(":" + port)
 }
